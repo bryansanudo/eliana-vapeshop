b658dd3899554eccae9bb204c89cb5131094e6e1
Bryan Sanudo Garcia <bryansanudo95@gmail.com>
2023-02-17 01:17:00 -0500

    navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,20 @@ import logoJmarioCouture from "@/assets/logoJmarioCouture.jpg";
 import logoMagicripped from "@/assets/logoMagicripped.jpg";
 import logoVoufit from "@/assets/logoVoufit.jpg";
 import logoVapeshop from "@/assets/logoVapeshop.jpg";
+import logoEliana from "@/assets/logoEliana1.jpg";
 
 const Navbar = ({ isMenuShown, setIsMenuShown }) => {
   const links = [
     {
-      id: 1,
-      link: "https://sara-marcas.vercel.app/",
-      src: logoSara,
+      id: 4,
+      link: "https://eliana-portafolio.vercel.app/",
+      src: logoEliana,
       brand:
-        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#6057ca] hover:shadow-[#6057ca] duration-300 cursor-pointer hover:shadow-xl shadow-md",
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-thPrimary hover:shadow-thPrimary duration-300 cursor-pointer hover:shadow-xl shadow-md",
     },
     {
-      id: 5,
-      link: "https://sara-marcas.vercel.app/",
+      id: 6,
+      link: "https://eliana-vapeshop.vercel.app/",
       src: logoVapeshop,
       brand:
         "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#35EEED] hover:shadow-[#35EEED] duration-300 cursor-pointer hover:shadow-xl shadow-md",
@@ -31,19 +32,20 @@ const Navbar = ({ isMenuShown, setIsMenuShown }) => {
       brand:
         "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#29fd04] hover:shadow-[#29fd04]   duration-300 cursor-pointer hover:shadow-xl shadow-md",
     },
+
     {
-      id: 3,
-      link: "https://sara-jmario.vercel.app/",
-      src: logoJmarioCouture,
-      brand:
-        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#fff] hover:shadow-[#fff] duration-300  cursor-pointer hover:shadow-xl shadow-md",
-    },
-    {
-      id: 4,
-      link: "https://sara-jmario.vercel.app/",
+      id: 5,
+      link: "https://sara-magicripped.vercel.app/",
       src: logoMagicripped,
       brand:
-        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-   hover:shadow-[#fd0101] duration-300  cursor-pointer hover:shadow-xl shadow-md",
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#fd0101] hover:shadow-[#fd0101] duration-300  cursor-pointer hover:shadow-xl shadow-md",
+    },
+    {
+      id: 1,
+      link: "https://sara-marcas.vercel.app/",
+      src: logoSara,
+      brand:
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#6057ca] hover:shadow-[#6057ca] duration-300 cursor-pointer hover:shadow-xl shadow-md",
     },
   ];
 
